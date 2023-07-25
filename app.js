303f81c34cbff228a0750d1b35b05d50508fd59c
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-07-25 17:52:07 +0530

    Level 3 Hahsing

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchems = new mongoose.Schema({
 });
 
 
-userSchems.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchems);
 
@@ -44,7 +45,7 @@ app.get("/register", (req, res)=>{
 app.post("/register", (req, res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save()
@@ -59,7 +60,7 @@ app.post("/register", (req, res)=>{
 
 app.post("/login", (req, res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
         .then((foundUser)=>{
